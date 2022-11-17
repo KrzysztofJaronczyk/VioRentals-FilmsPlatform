e2f28ed3c2ea3c942ce7058bdad3d34c682de844
Krzysztof Jarończyk <krzysztof.jaronczyk@microsoft.wsei.edu.pl>
2022-11-17 11:49:28 +0100

    Dodaj pliki projektów.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace VioRentals___platforma_z_filmami.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
